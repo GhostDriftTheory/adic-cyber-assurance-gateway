c45da9fe9800cfcbf9608acb2caffed693105a9b
GhostDriftTheory <hide.pollini.kenkou@gmail.com>
2026-05-20 07:20:59 +0900

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,10 @@
 import Lake
 open Lake DSL
 
-package «adic_lean_proof_replay» where
+package «adic_cyber_assurance_gateway» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
 
 @[default_target]
-lean_lib «ADIC_RSound_Replay» where
+lean_lib «adic_cyber_assurance_gateway» where
